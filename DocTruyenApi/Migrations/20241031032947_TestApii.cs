2c92a978e98c85a4889841cff2223d8c2327e114
Mon1712 <133070020+Mon1712@users.noreply.github.com>
2024-11-04 11:15:09 +0700

    test

---
+++ DocTruyenApi/Migrations/20241031032947_TestApii.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocTruyenApi.Migrations
+{
+    public partial class TestApii : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
