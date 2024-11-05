bf82373c42531b840a4ec14caada0ab10f130040
Mon1712 <133070020+Mon1712@users.noreply.github.com>
2024-11-05 18:10:24 +0700

    comment update

--- DocTruyenApi/Controllers/CommentsController.cs
+++ DocTruyenApi/Controllers/CommentsController.cs
@@ -1,4 +1,5 @@
-﻿using DocTruyenApi.Models;
+﻿using DocTruyenApi.DTOs;
+using DocTruyenApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,29 +18,46 @@ namespace DocTruyenApi.Controllers
             _context = context;
         }
         // GET: api/<CommentsController>
-        [HttpGet("getComments/{bookId}")]
-        public IActionResult GetComments(int accountId, int bookId)
-        {
-            // Fetch comments based on accountId and bookId
-            var comments = _context.Comments
-                .Where(c => c.User.AccountId == accountId && c.BookId == bookId)
-                .Select(c => new
-                {
-                    CommentId = c.CommentId,
-                    Content = c.Content,
-                 
-                    UserName = c.User.UserName 
-                })
-                .ToList();
 
-            if (comments == null || !comments.Any())
-            {
-                return NotFound("No comments found for this book and user.");
-            }
+        [HttpGet("getComments/{bookId}")]
+        public IActionResult GetComments(int bookId)
+        {
+            // Giả sử bạn đã có một context cho cơ sở dữ liệu
+            var comments = _context.Comments
+                .Where(c => c.BookId == bookId)
+                .Include(c => c.User) // Bao gồm thông tin người dùng
+                .ToList();
 
             return Ok(comments);
         }
-
+        [HttpPost("addComment/{accountId}")]
         
+        public IActionResult AddComment([FromBody] CommentDTO commentDto, int accountId)
+        {
+            // Retrieve the user associated with the accountId
+            var user = _context.Users.FirstOrDefault(u => u.AccountId == accountId);
+
+            if (user == null)
+            {
+                return NotFound("User with the specified accountId does not exist.");
+            }
+
+            // Create a new Comment object
+            var comment = new Comment
+            {
+                Content = commentDto.Content,
+                BookId = commentDto.BookId,
+                UserId = user.UserId 
+            };
+
+            
+            _context.Comments.Add(comment);
+            _context.SaveChanges();
+
+            return Ok("Comment added successfully.");
+        }
+
+
+
     }
 }
---
+++ DocTruyenApi/DTOs/CommentDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DocTruyenApi.DTOs
+{
+    public class CommentDTO
+    {
+        public int? CommentId { get; set; }
+        public string? Content { get; set; }
+      
+        //public int? UserId { get; set; }
+        
+        public int BookId { get; set; }
+    }
+}
